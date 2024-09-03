288d0375a612061179805ddbbc83732e86af3ed7
pahnupong660610783 <pahnupong_kaeopramun@cmu.ac.th>
2024-09-04 00:55:24 +0700

    1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,9 +34,9 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Footer
-            year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            year ="2024"
+            fullName="Pahnupong Kaeopramun"
+            studentId="660610783"
           />
         </div>
       </body>
